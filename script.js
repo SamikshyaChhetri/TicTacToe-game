5a87e1561872e898b1853fe70e006b8c6958aed4
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-06-30 23:32:26 +0545

    Font size modified

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// const first = document.getElementById("first");
-// first.addEventListener("click", () => {
-//   first.innerHTML = "X";
-// });
 const classes = document.querySelectorAll(".className");
 classes.forEach((e) => {
   e.addEventListener("click", () => {
