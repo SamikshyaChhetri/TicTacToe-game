8278a896c365c35394f7af3880d08060704ab755
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-06-30 23:48:23 +0545

    CSS file linked

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ const classes = document.querySelectorAll(".className");
 classes.forEach((e) => {
   e.addEventListener("click", () => {
     e.innerHTML = "X";
+    e.classList.add("clicked");
   });
 });
