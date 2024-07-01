4793f9536e4e19b9ad43f5ad3721cd42609e5f11
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-07-01 22:33:44 +0545

    Array for win condition

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
+const winConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+const oSelected = [];
+const xSelected = [];
 const classes = document.querySelectorAll(".className");
+let turn = "X";
 classes.forEach((e) => {
   e.addEventListener("click", () => {
-    e.innerHTML = "X";
-    e.classList.add("clicked");
+    if (turn === "X") {
+      e.innerHTML = "O";
+      turn = "O";
+    } else {
+      e.innerHTML = "X";
+      turn = "X";
+    }
   });
 });
