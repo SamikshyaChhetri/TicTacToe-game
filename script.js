2606de587f1fb38f6aec9ae953c9ef9cf3b6d153
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-07-03 23:12:41 +0545

    Tic tac toe completed

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const winConditions = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
+  ["0", "1", "2"],
+  ["3", "4", "5"],
+  ["6", "7", "8"],
+  ["0", "3", "6"],
+  ["1", "4", "7"],
+  ["2", "5", "8"],
+  ["0", "4", "8"],
+  ["2", "4", "6"],
 ];
+
 const oSelected = [];
 const xSelected = [];
 const classes = document.querySelectorAll(".className");
+const displayResult = document.getElementById("display");
 let turn = "X";
 classes.forEach((e) => {
   e.addEventListener("click", () => {
@@ -19,25 +21,31 @@ classes.forEach((e) => {
       const element = e.getAttribute("data-index");
       oSelected.push(element);
       console.log(oSelected);
-      turn = "O";
       checkWinner();
+      turn = "O";
     } else {
       e.innerHTML = "X";
       const element = e.getAttribute("data-index");
       xSelected.push(element);
       console.log(xSelected);
-      turn = "X";
       checkWinner();
+      turn = "X";
     }
   });
 });
+
 function checkWinner() {
   winConditions.forEach((condition) => {
     if (condition.every((item) => oSelected.includes(item))) {
       console.log("O is the winner");
+      displayResult.innerHTML = "O is the winner";
+      displayResult.classList.remove("hidden");
       return;
     } else if (condition.every((item) => xSelected.includes(item))) {
       console.log("X is the winner");
+      displayResult.innerHTML = "X is the winner";
+      displayResult.classList.remove("hidden");
+
       return;
     }
   });
